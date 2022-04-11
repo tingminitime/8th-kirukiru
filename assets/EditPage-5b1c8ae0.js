f7fbb9bef41c1976aa2b1ca95fd5300ed784262f
Tim Lin <e0610k@gmail.com>
2022-04-12 02:36:22 +0800

    delete:dist

--- assets/EditPage-5b1c8ae0.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-77469bef.js";import{r as e,o as a,c as o,e as s}from"./vendor-358810ac.js";const t={class:"pb-24"};var n=r({name:"EditorPage"},[["render",function(r,n,i,c,d,f){const m=e("router-view");return a(),o("div",t,[s(m)])}]]);export{n as default};
