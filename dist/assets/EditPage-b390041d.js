aad2cec7b332575c22978f1205f015eb4c2a3729
tingminitime <e0610k@gmail.com>
2022-04-12 12:56:45 +0800

    update:dist to github pages

---
+++ dist/assets/EditPage-b390041d.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-ce9f5a0d.js";import{r as a,o as e,c as o,e as s}from"./vendor-358810ac.js";const t={class:"pb-24"};var n=r({name:"EditorPage"},[["render",function(r,n,c,d,i,f){const m=a("router-view");return e(),o("div",t,[s(m)])}]]);export{n as default};
